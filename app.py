8fc1f1abf4046b60ab605803ae4b7ac245450144
Mad-Mange <madheli@hotmail.com>
2021-04-19 11:06:52 +0200

    werwer

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import os
 
 app = Flask(__name__)
 
+port = int(os.environ.get("PORT", 5000))
 @app.route('/')
-def hello():
-  return "Hello World!"
+def hello_world():
+    return 'Flask Dockerized and deployed to Heroku'
+
+def add(a, b):
+    return a + b
 
 if __name__ == '__main__':
-  port = int(os.environ.get('PORT', 5000))
-  app.run(host = '0.0.0.0', port = port)
+    app.run(debug=True,host='0.0.0.0',port=port)
